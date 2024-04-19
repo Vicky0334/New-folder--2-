94d54a19d7ade8f831df1e87f02a699e6584ab1b
Vicky Dhote <dhoteji034@gmail.com>
2024-04-20 00:17:48 +0530

    mongodb

---
+++ models/connect.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+exports.mongoconnection=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGOURL);
+        console.log('Connected to MongoDB');
+        }
+        
+    catch(err){
+        console.log( err.message);
+    }
+};
