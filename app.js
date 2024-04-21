2afe3d6beedee4039618a1d55da2b5a09df199d9
Vicky Dhote <dhoteji034@gmail.com>
2024-04-21 23:19:30 +0530

    404 error message

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ const indexRouter = require('./routes/indexRoutes');
 
 //setting logger
 app.use(require('morgan')("tiny"));
+
 //bodyparser
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
 
 //router setup->must be second last in the file
+
+
 app.use("api/user",indexRouter);
+app.all("*",(req,res) => {
+    res.status(404).json({ url:req.url, message:"Page not found"});
+});
+
+
 // creating  server   ---must be at the last line
 app.listen(process.env.PORT,()=>{
     console.log(`Server is run ${process.env.PORT}`);
