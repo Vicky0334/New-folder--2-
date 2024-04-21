a92d9568acf3ae2eaf31df9a5ceacd3b7e7a356d
Vicky Dhote <dhoteji034@gmail.com>
2024-04-21 23:20:19 +0530

    creat new route

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,24 +1,9 @@
 const express=require('express');
 const router = express.Router();
 const User=require("../models/userModel");
+const{home,register,readall}=require("../controllers/indexController");
 
-/**
- * @route /api/user/
- * @access public
- * @method get
- */
-router.get('/',(req,res,next)=>{
-    res.status(202).json({success:true,message:"this is test message"});
-   
-});
-router.post('/register',async(req,res,next)=>{
-    try {
-        const user =new User(req.body);
-         await user.save();
-        res.status(201).json({success:true,user});
-    } catch (error) {
-        res.status(500).json({success:false,error:error.message});
-        
-    }
-}); 
+router.get('/',home);
+router.post('/register',register); 
+router.post('/readall',readall); 
 module.exports=router;
